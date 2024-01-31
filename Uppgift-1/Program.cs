88eb50c362f10c12721dce761eca588dff943b95
Admin <Admin@TEB123018>
2024-01-31 10:44:21 +0100

    Add project files.

---
+++ Uppgift-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namnLista = new();
+            string input = " ";
+
+            Console.WriteLine("Välkommnen, skriv in så många namn som du vill");
+            Console.WriteLine("När du är nöjd så trycker du enter och skriver en tom rad för att skriva ut alla namn");
+
+            while (input != "")
+            {
+                Console.WriteLine();
+                Console.Write("Skriv in namn:");
+                input = Console.ReadLine();
+                namnLista.Add(input);
+            }
+
+            namnLista.Sort();
+
+            Console.WriteLine("Namnen du skrev in i omvänd bokstavsordning");
+            for (int i = namnLista.Count() - 1; i > 0; i--) 
+            {
+                Console.WriteLine(namnLista[i]);
+            }
+        }
+    }
+}
